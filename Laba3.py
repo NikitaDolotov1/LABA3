0f0cf3c32d6e0f068586813480410acbebda414a
NikitaDolotov1 <dolotov-nikitka@mail.ru>
2023-03-24 23:00:24 +0300

    add file

---
+++ Laba3.py
@@ -0,0 +1,28 @@
+class Hero:
+    def __init__(self, name, lvl, money):
+        self._name = name
+        self._lvl = lvl
+        self._money = money
+
+    def setName(self, name):
+        self._name = name
+
+    def setLvl(self, lvl):
+        self._lvl = lvl
+
+    def setMoney(self, money):
+        self._money = money
+
+    def getName(self):
+        return self._name
+
+    def getLvl(self):
+        return self._lvl
+
+    def getMoney(self):
+        return self._money
+
+
+h = Hero("Storm Spirit", 25 , 22600)
+
+print(f'Имя: {h._name}, Уровень: {h._lvl}, Деньги: {h._money}')
